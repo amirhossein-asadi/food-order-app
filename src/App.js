e36105f61e4ad832c02f2a5efa00553b4cb2e420
Amirhossein Asadi <38536305+amirhossein-asadi@users.noreply.github.com>
2021-08-01 15:33:52 +0430

    Add to cart finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+
 import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 const App = () => {
   const [isShowCart, setIsShowCart] = useState(false);
@@ -15,13 +17,13 @@ const App = () => {
   };
 
   return (
-    <React.Fragment>
+    <CartProvider>
       {isShowCart && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </React.Fragment>
+    </CartProvider>
   );
 };
 
